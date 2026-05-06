dec2b3d16e8c9195c03966556b8f473c0e20ecb2
StudentX 69 <nabhay114@gmail.com>
2026-05-06 22:22:59 +0530

    bug fix

--- converter.js
+++ converter.js
@@ -1,25 +1,20 @@
 const calculateTemp = () => {
-    const NumberTemp = document.getElementById('temp').value;
-    // 
-    // const tempslected = document.getElementById('temp_diff').value;
-    const valueTemp = temp_diff.options[tempslected.selectedIndex].value;
-    const celToFah = (cel) => {
-        let fahrenheit = Math.round((cel * (9 / 5)) + 32);
-        return fahrenheit;
-    }
-    const FahTocel = (fah) => {
-        let celcius = Math.round((fah - 32) * (5 / 9));
-        return celcius;
-    }
+    const NumberTemp = parseFloat( document.getElementById('temp').value);
+    const tempslected = document.getElementById('temp_diff');
+    const valueTemp = tempslected.value;
+
+    const celToFah = (cel) =>  Math.round((cel * (9 / 5)) + 32);
+    const fahTocel = (fah) => Math.round((fah - 32) * (5 / 9));
     // let result = celToFah(NumberTemp);
     //     document.querySelector('#resultcontainer').innerHTML = `${result}°fahrenheit`;
     
+    // let result = FahTocel(NumberTemp);
+    //    document.querySelector('#resultcontainer').innerHTML = `${result}°Celsius`;
     if (valueTemp == 'cel') {
         result = celToFah(NumberTemp);
-        document.querySelector('#resultcontainer').innerHTML = `${result}°celsius`;
+        document.querySelector('#resultcontainer').innerHTML = `${result}°Fahrenheit`;
     } else {
         result = fahTocel(NumberTemp);
-        document.querySelector('#resultcontainer').innerHTML = `${result}°fahrenheit`;
+        document.querySelector('#resultcontainer').innerHTML = `${result}°Celsius`;
     }
-
 } 
